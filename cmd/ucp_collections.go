ee8824d1d8843119633deeb20915f0ff83d0db9a
Dan Finneran <daniel.finneran@gmail.com>
2018-07-11 13:54:52 +0100

    Additional Functionality around collections
    - list (with formatting)
    - create
    - get (retrieve details of a collection

--- cmd/ucp_collections.go
+++ cmd/ucp_collections.go
@@ -5,15 +5,29 @@ import (
 	"os"
 	"text/tabwriter"
 
+	"github.com/thebsdbox/diver/pkg/ucp/types"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/thebsdbox/diver/pkg/ucp"
 )
 
+var newCollection ucptypes.Collection
+var labelConstraints []string
+var parentID string
+
 func init() {
+	ucpCollectionsGet.Flags().StringVar(&name, "id", "", "The ID of collection to inspect")
+
+	ucpCollectionsCreate.Flags().StringVar(&name, "name", "", "Name of new collection")
+	ucpCollectionsCreate.Flags().StringVar(&parentID, "parent", "", "The ID of the parent collection")
 
 	ucpAuth.AddCommand(ucpCollections)
+
+	ucpCollections.AddCommand(ucpCollectionsCreate)
 	ucpCollections.AddCommand(ucpCollectionsList)
+	ucpCollections.AddCommand(ucpCollectionsGet)
+
 }
 
 var ucpCollections = &cobra.Command{
@@ -45,6 +59,70 @@ var ucpCollectionsList = &cobra.Command{
 
 		for i := range collections {
 			fmt.Fprintf(w, "%s\t%s\n", collections[i].Name, collections[i].ID)
+		}
+		w.Flush()
+	},
+}
+
+var ucpCollectionsCreate = &cobra.Command{
+	Use:   "create",
+	Short: "Create a Docker EE Collection",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.SetLevel(log.Level(logLevel))
+		if name == "" {
+			cmd.Help()
+			log.Fatalf("No collection ID specified")
+		}
+		if parentID == "" {
+			cmd.Help()
+			log.Fatalf("No parent collection ID specified")
+		}
+		client, err := ucp.ReadToken()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		err = client.CreateCollection(name, parentID)
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+	},
+}
+
+var ucpCollectionsGet = &cobra.Command{
+	Use:   "get",
+	Short: "Get information about a Docker EE Collection",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.SetLevel(log.Level(logLevel))
+		if name == "" {
+			cmd.Help()
+			log.Fatalf("No collection ID specified")
+		}
+		client, err := ucp.ReadToken()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		collection, err := client.GetCollection(name)
+		if collection == nil {
+			log.Fatalf("Error parsing collection [%s]", name)
+		}
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+		const padding = 3
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', 0)
+
+		fmt.Fprintf(w, "Name\t%s\n", collection.Name)
+		fmt.Fprintf(w, "ID\t%s\n", collection.ID)
+		fmt.Fprintf(w, "Path\t%s\n", collection.Path)
+		fmt.Fprintf(w, "Parents\t\n")
+
+		for i := range collection.ParentIds {
+			fmt.Fprintf(w, "\t/%s\n", collection.ParentIds[i])
+		}
+		fmt.Fprintf(w, "Label Constraints\t\n")
+		fmt.Fprintf(w, "\tKey\tValue\tEqual to\tType\n")
+		for i := range collection.LabelConstraints {
+			fmt.Fprintf(w, "\t%s\t%s\t%t\t%s\n", collection.LabelConstraints[i].LabelKey, collection.LabelConstraints[i].LabelValue, collection.LabelConstraints[i].Equality, collection.LabelConstraints[i].Type)
 		}
 		w.Flush()
 	},
--- pkg/ucp/ucpCollections.go
+++ pkg/ucp/ucpCollections.go
@@ -23,7 +23,7 @@ func (c *Client) returnAllCollections() ([]byte, error) {
 	return response, nil
 }
 
-// GetCollections - This will get all accounts
+// GetCollections - This will get all collections
 func (c *Client) GetCollections() ([]ucptypes.Collection, error) {
 	data, err := c.returnAllCollections()
 
@@ -34,4 +34,48 @@ func (c *Client) GetCollections() ([]ucptypes.Collection, error) {
 		return nil, err
 	}
 	return collections, nil
+}
+
+// CreateCollection - This will get all accounts
+func (c *Client) CreateCollection(name, parentID string) error {
+	data := map[string]string{
+		"name":      name,
+		"parent_id": parentID,
+	}
+
+	b, err := json.Marshal(data)
+
+	if err != nil {
+		return err
+	}
+
+	url := fmt.Sprintf("%s/collections", c.UCPURL)
+
+	log.Debugf("Built URL [%s]", url)
+	_, err = c.postRequest(url, b)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetCollection - This will get all accounts
+func (c *Client) GetCollection(collectionID string) (*ucptypes.Collection, error) {
+	// Build the URL (TODO set limit)
+
+	url := fmt.Sprintf("%s/collections/%s", c.UCPURL, collectionID)
+
+	log.Debugf("Built URL [%s]", url)
+	response, err := c.getRequest(url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	foundCollection := ucptypes.Collection{}
+
+	err = json.Unmarshal(response, &foundCollection)
+	if err != nil {
+		return nil, err
+	}
+	return &foundCollection, nil
 }
