68118dc60dd060bb4cb9457f63ea56e7e9301bb4
Dan Finneran <daniel.finneran@gmail.com>
2018-08-13 14:56:15 +0100

    Set Node orchestrator
    Ability to set which orchestrator can manage a node:
    
    ```
    diver ucp nodes orchestrator --id d5kqmkg5elaq1ygk6nf7qzmer --swarm --kubernetes
    INFO[0000] Configured Node [d5kqmkg5elaq1ygk6nf7qzmer] to use orchestrator kubernetes=[true] / swarm=[true]
    ```

--- cmd/ucp_nodes.go
+++ cmd/ucp_nodes.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"text/tabwriter"
 
 	log "github.com/Sirupsen/logrus"
@@ -10,11 +11,19 @@ import (
 	"github.com/thebsdbox/diver/pkg/ucp"
 )
 
+// Used to enable or disable orchestrator type
+var orchestratorKube, orchestratorSwarm bool
+
 func init() {
 	ucpNodesGet.Flags().StringVar(&id, "id", "", "ID of the Docker Node")
 
+	ucpNodesOrchestrator.Flags().StringVar(&id, "id", "", "ID of the Docker Node")
+	ucpNodesOrchestrator.Flags().BoolVar(&orchestratorKube, "kubernetes", false, "Enable Kubernetes to use this node")
+	ucpNodesOrchestrator.Flags().BoolVar(&orchestratorSwarm, "swarm", false, "Enable Swarm to use this node")
+
 	ucpNodes.AddCommand(ucpNodesList)
 	ucpNodes.AddCommand(ucpNodesGet)
+	ucpNodes.AddCommand(ucpNodesOrchestrator)
 
 	// Add nodes to UCP root commands
 	UCPRoot.AddCommand(ucpNodes)
@@ -66,7 +75,10 @@ var ucpNodesGet = &cobra.Command{
 	Short: "Get information about a particular Docker Node",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.SetLevel(log.Level(logLevel))
-
+		if id == "" {
+			cmd.Help()
+			log.Fatalln("No Node ID specified")
+		}
 		client, err := ucp.ReadToken()
 		if err != nil {
 			// Fatal error if can't read the token
@@ -78,8 +90,40 @@ var ucpNodesGet = &cobra.Command{
 			log.Fatalf("%v", err)
 		}
 		log.Debugf("Retrieved information about [%s]", node.Description.Hostname)
+
+		w := tabwriter.NewWriter(os.Stdout, 0, 0, tabPadding, ' ', 0)
+		fmt.Fprintln(w, "Label Key\tLabel Value")
 		for k, v := range node.Spec.Labels {
-			fmt.Printf("%s / %s\n", k, v)
+			fmt.Fprintf(w, "%s\t%s\n", k, v)
 		}
+		w.Flush()
+	},
+}
+
+var ucpNodesOrchestrator = &cobra.Command{
+	Use:   "orchestrator",
+	Short: "Enable node for different orchestrators",
+	Run: func(cmd *cobra.Command, args []string) {
+		log.SetLevel(log.Level(logLevel))
+		if id == "" {
+			cmd.Help()
+			log.Fatalln("No Node ID specified")
+		}
+		client, err := ucp.ReadToken()
+		if err != nil {
+			// Fatal error if can't read the token
+			log.Fatalf("%v", err)
+		}
+		err = client.SetNodeLabel(id, "com.docker.ucp.orchestrator.kubernetes", strconv.FormatBool(orchestratorKube))
+		if err != nil {
+			// Fatal error if can't read the token
+			log.Fatalf("%v", err)
+		}
+		err = client.SetNodeLabel(id, "com.docker.ucp.orchestrator.swarm", strconv.FormatBool(orchestratorSwarm))
+		if err != nil {
+			// Fatal error if can't read the token
+			log.Fatalf("%v", err)
+		}
+		log.Infof("Configured Node [%s] to use orchestrator kubernetes=[%t] / swarm=[%t]", id, orchestratorKube, orchestratorSwarm)
 	},
 }
--- pkg/ucp/ucpNodes.go
+++ pkg/ucp/ucpNodes.go
@@ -42,11 +42,38 @@ func (c *Client) GetNode(id string) (swarm.Node, error) {
 		return node, err
 	}
 
-	log.Debugf("Parsing all nodes")
+	log.Debugf("Parsing Node details")
 	err = json.Unmarshal(response, &node)
 	if err != nil {
 		return node, err
 	}
 
 	return node, nil
+}
+
+//SetNodeLabel - Retrieves the complete list of all nodes connected to a UCP cluster
+func (c *Client) SetNodeLabel(id, k, v string) error {
+
+	log.Debugln("Retrieving information about existing configuration")
+	node, err := c.GetNode(id)
+	if err != nil {
+		return err
+	}
+
+	// Modify the node spec labels
+	node.Spec.Labels[k] = v
+
+	b, err := json.Marshal(node.Spec)
+	if err != nil {
+		return err
+	}
+	log.Debugf("%s", b)
+	url := fmt.Sprintf("%s/nodes/%s/update?version=%d", c.UCPURL, id, node.Version.Index)
+
+	response, err := c.postRequest(url, b)
+	if err != nil {
+		ParseUCPError(response)
+		return err
+	}
+	return nil
 }
